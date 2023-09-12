c3bd15c7d93a00f00fad0449bb50a41357af4758
Denis Gukov <denis.guk@konomic.com>
2023-09-12 09:44:10 +0300

    Fixed pagination pages logic

--- src/const/index.ts
+++ src/const/index.ts
@@ -44,39 +44,56 @@ const range = (from: number, to: number): number[] => {
 }
 
 const paginationItems = (value: number, length: number, total: number): Array<number | string> => {
-    const totalVisible = value <= 3 || value >= length - 2 ? 5 : total;
-    const maxLength = Math.min(Math.max(0, totalVisible) || length, length);
-
-    if (length <= maxLength) {
-        return range(1, length);
-    }
-
-    const even = maxLength % 2 === 0 ? 1 : 0;
-    const left = Math.floor(maxLength / 2);
-    const right = length - left + 1 + even;
-
-    if (value > left && value < right) {
-        console.log(1);
-        const firstItem = 1;
-        const lastItem = length;
-        const start = value - left + 2;
-        const end = value + left - 2 - even;
-        const secondItem = start - 1 === firstItem + 1 ? 2 : '...';
-        const beforeLastItem = end + 1 === lastItem - 1 ? end + 1 : '...';
-
-        return [1, secondItem, ...range(start, end), beforeLastItem, length];
-    } else if (value === left) {
-        console.log(1);
-        const end = value + left - 1 - even;
-        return [...range(1, end), '...', length];
-    } else if (value === right) {
-        console.log(1);
-        const start = value - left + 1;
-        return [1, '...', ...range(start, length)];
+    let first;
+    let current = value;
+    let last;
+    
+    if (value !== 1 && value !== length) {
+        first = value - 1;
+        last = value + 1;
     } else {
-        console.log(1);
-        return [...range(1, left), '...', ...range(right, length)];
+        value === 1 ? last = value + 1 : first = value - 1;
     }
+
+    const result: Array<number> = [];
+    first && result.push(first);
+    result.push(current);
+    last && result.push(last);
+
+    return result;
+    // const totalVisible = value <= 3 || value >= length - 2 ? 5 : total;
+    // const maxLength = Math.min(Math.max(0, totalVisible) || length, length);
+
+    // if (length <= maxLength) {
+    //     return range(1, length);
+    // }
+
+    // const even = maxLength % 2 === 0 ? 1 : 0;
+    // const left = Math.floor(maxLength / 2);
+    // const right = length - left + 1 + even;
+
+    // if (value > left && value < right) {
+    //     console.log(1);
+    //     const firstItem = 1;
+    //     const lastItem = length;
+    //     const start = value - left + 2;
+    //     const end = value + left - 2 - even;
+    //     const secondItem = start - 1 === firstItem + 1 ? 2 : '...';
+    //     const beforeLastItem = end + 1 === lastItem - 1 ? end + 1 : '...';
+
+    //     return [1, secondItem, ...range(start, end), beforeLastItem, length];
+    // } else if (value === left) {
+    //     console.log(1);
+    //     const end = value + left - 1 - even;
+    //     return [...range(1, end), '...', length];
+    // } else if (value === right) {
+    //     console.log(1);
+    //     const start = value - left + 1;
+    //     return [1, '...', ...range(start, length)];
+    // } else {
+    //     console.log(1);
+    //     return [...range(1, left), '...', ...range(right, length)];
+    // }
 }
 
 export { characters, tableHeaders, filterArray, paginationItems };
